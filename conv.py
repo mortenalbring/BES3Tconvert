ed80f02a6f633e783f9edb07a4c0a6c303f022fb
Morten Albring <mortenalbring@gmail.com>
2013-07-02 18:50:26 +0100

    Added command line arguments and error checks

--- conv.py
+++ conv.py
@@ -1,44 +1,76 @@
-import codecs
-##f = codecs.open('GT192_10_5Kb.DTA','r','utf-32LE');
-
-##f.readline();
-
-##f = open('g.DTA','rb');
-##
-##try:
-##    byte = f.read(1)
-##    while byte != "":
-##        byte = f.read(1)
-##        print(byte);
-##finally:
-##    f.close()
-##        
-##import binascii
-##print(binascii.crc32("hello"));
+#!/usr/bin/python
 
 import struct
+import sys
+import getopt
+import os
 
-fin = open('V1P45.spc','rb');
+def main(argv):
+    inputfile = ''
+    outputfile = ''
 
-fout = open('test.txt','w');
-
-toggle = 1;
-
-try:
-    b = struct.unpack('>f',fin.read(4));
-    while b != "":
-        b = struct.unpack('>f',fin.read(4));
-        if toggle == 1 :
-            print(b[0])
-            print(',')
-            fout.write(str(b[0]));
-            fout.write(',');
-            toggle = 2;
-        else :
-            print(b[0])
-            fout.write(str(b[0]));
-            fout.write('\n');
-            toggle = 1;
+#Get command line arguments
+    
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print("Oh no! You didn't put in enough arguments or something!")
+        sys.exit(2)
+    for opt,arg in opts:
+        if opt == '-h':
+            print("conv.py -i inputfile -o outputfile");
+            sys.exit()
+        elif opt in ("-i","--ifile"):
+            inputfile = arg
+        elif opt in ("-o","--ofile"):
+            outputfile = arg
+#Test if input and output files have been specified properly
             
-finally:
-    fin.close()
+    if inputfile == '' :
+        print("Oh no! You haven't specified an input file!")
+        print("conv.py -i inputfile -o outputfile")
+        sys.exit(2)
+    elif outputfile == '' :
+        print("Oh no! You haven't specified an output file!")
+        print("conv.py -i inputfile -o outputfile")
+        sys.exit(2)
+
+#Test if input file exists
+    try:
+        with open(inputfile) : pass
+    except IOError:
+        print("input file doesn't exist!")
+
+#Test if file is DTA
+        
+    fileName, fileExtension = os.path.splitext(inputfile)
+    print(fileExtension)
+    if fileExtension == ".DTA" :
+        print("Now converting DTA file!");
+
+        fin = open(inputfile,'rb');
+        fout = open(outputfile,'w');
+
+        toggle = 1;
+        try:
+            b = struct.unpack('>f',fin.read(4));
+            while b != "":
+                b = struct.unpack('>f',fin.read(4));
+                if toggle == 1 :
+                    print(b[0])
+                    print(',')
+                    fout.write(str(b[0]));
+                    fout.write(',');
+                    toggle = 2;
+                else :
+                    print(b[0])
+                    fout.write(str(b[0]));
+                    fout.write('\n');
+                    toggle = 1;
+                    
+        finally:
+            fin.close()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
